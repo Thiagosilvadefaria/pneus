9a490433ebd79894e14c39e320cbfb5ecac414c7
Thiagosilvadefaria <thieloim@gmail.com>
2026-02-11 18:07:54 -0300

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -92,7 +92,7 @@ const Footer: React.FC = () => {
             <div className="space-y-3 mb-6">
               <div className="flex items-center space-x-3">
                 <Phone className="w-5 h-5 text-red-500" />
-                <span className="text-gray-300">(13) 99742-9361</span>
+                <span className="text-gray-300">(13) 999999999</span>
               </div>
               <div className="flex items-center space-x-3">
                 <Mail className="w-5 h-5 text-red-500" />
